12af214968507054cb76ed934c4005151012ae87
jonas <jonas.langenberg@outlook.de>
2024-07-22 22:13:51 +0200

    Finished task

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(startNumber, endNumber) {
+    if (startNumber > endNumber){
+        let placeholder = startNumber;
+        startNumber = endNumber;
+        endNumber = placeholder;
+    }
 
+    if(Number.isInteger(startNumber) == false || Number.isInteger(endNumber) == false){
+        return "ERROR";
+    }
+
+    if(startNumber < 0 || endNumber < 0){
+        return "ERROR";
+    }
+    let sum = 0;
+    for(let i = startNumber; i <= endNumber; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
