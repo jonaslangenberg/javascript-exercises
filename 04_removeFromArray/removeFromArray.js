7ef58231f0cee7c9ecc62d5fd53a510a0ffdd36b
jonas <jonas.langenberg@outlook.de>
2024-07-22 21:48:01 +0200

    Commit to save

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayToAlter) {
+    let returnArray = [];
+    
+    outer: for(const item of arrayToAlter){
+        for(let i = arguments.length - 1; i < arguments.length; i++){
+            if(arguments[i] == item){
+                continue outer;
+            }
+        }
+        returnArray.push(item);
+    }
 };
 
 // Do not edit below this line
