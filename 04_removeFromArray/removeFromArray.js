8c3283d1661ed14d1305d5622deaf112fc22ce98
jonas <jonas.langenberg@outlook.de>
2024-07-22 21:58:24 +0200

    Change condition in inner for loop

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,14 +1,15 @@
-const removeFromArray = function(arrayToAlter) {
+const removeFromArray = function() {
     let returnArray = [];
     
-    outer: for(const item of arrayToAlter){
-        for(let i = arguments.length - 1; i < arguments.length; i++){
-            if(arguments[i] == item){
+    outer: for(let item of arguments[0]){
+        for(let i = 1; i < arguments.length; i++){
+            if(arguments[i] === item){
                 continue outer;
             }
         }
         returnArray.push(item);
     }
+    return returnArray;
 };
 
 // Do not edit below this line
